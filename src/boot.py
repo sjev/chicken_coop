b40112d4307ffd543bfdce905cabd0b9ce17c8eb
Jev Kuznetsov <jev.kuznetsov@gmail.com>
2019-03-17 22:25:06 +0100

    added a copy of robust mqtt client, now with reset condition

--- src/boot.py
+++ src/boot.py
@@ -8,43 +8,42 @@ import time
 import gc
 
 
-def connectWifi(networks=config.networks):
+def connectWifi():
     """ connect to a wifi network """
     ap_if = network.WLAN(network.AP_IF)
     ap_if.active(False) # disable access point
-    
-    sta_if = network.WLAN(network.STA_IF)
-    
-    if not sta_if.isconnected():
-        for essid,password in networks:
-            print('connecting to ',essid)
-            sta_if.active(True)
-            
-            sta_if.connect(essid, password)
-            for retry in range(5):
-                if sta_if.isconnected():
-                    break
-                else:
-                    print('Retrying #',retry)
-                    time.sleep(3)
-            if sta_if.isconnected():
-                    break
-    
-    if sta_if.isconnected(): # should be connected now
+
+    wifi = network.WLAN(network.STA_IF)
+
+    if not wifi.isconnected():
         
-        # change to predefined ip address
-        sta_if.ifconfig(config.netconfig)
-        sta_if.connect()
+        print('connecting to ',config.ssid)
+    
+        #wifi.ifconfig(config.netconfig)
+        wifi.active(True)
+        wifi.connect(config.ssid, config.wifiPass)
+        for retry in range(5):
+            if wifi.isconnected():
+                break
+            else:
+                print('Retrying #',retry)
+                time.sleep(3)
+        if not wifi.isconnected(): # could not connect
+            print('Could not connect, rebooting')
+            time.sleep(2)
+            machine.reset()
+
+    if wifi.isconnected(): # should be connected now
+        print('network config:', wifi.ifconfig())
         
-        print('network config:', sta_if.ifconfig())
     else: # warn with led and reboot
         print('Connection failed!')
         time.sleep(5)
         print('Restarting')
         machine.reset()
-        
 
-connectWifi(networks = config.networks)
+
+connectWifi()
 gc.collect()
 # start webrepl
 import webrepl
--- src/main.py
+++ src/main.py
@@ -1,20 +1,22 @@
 from machine import Pin
 import time
 import config
-from umqtt.simple import MQTTClient
+from mqtt import MQTTClient
 import ntptime
 
+KEEP_ALVIVE = 20 # ping every such seconds
+
 class Board:
-    
+
     # name - gpio pairs
     outputs = [('led',2),('d1',5),('d2',4)]
-    
+
     def __init__(self):
 
         self.pins = {}
         for name,gpio in self.outputs:
             self.pins[name] = Pin(gpio, Pin.OUT)
-       
+
     def off(self,pinName):
         self.pins[pinName].value(0)
 
@@ -27,11 +29,11 @@ class Board:
     def motorUp(self):
         self.on('d1')
         self.off('d2')
-    
+
     def motorDown(self):
         self.on('d2')
         self.off('d1')
-    
+
     def motorOff(self):
         self.off('d1')
         self.off('d2')
@@ -41,53 +43,61 @@ def mqttCallback(topic,msg):
 
     global client
 
-    if topic == b'coop/door/cmd': # received command
-        client.publish(b'coop/door/action',msg) # echo command
-        
-        
+    if topic == b'cmnd/coop/DOOR': # received command
+        client.publish(b'stat/coop/DOOR',msg) # echo command
+
+
+
+
 def printInfo():
     """ print diagnostic info to serial """
     from network import WLAN
     wifi = WLAN()
-    
-      
-    
+
     print('ifconfig :', wifi.ifconfig())
     print('rssi: ',wifi.status('rssi'))
 
 def mainLoop(client,board):
-    
+
     counter = 0
-    client.publish('coop/status','online')
+
 
     while True:
         board.toggle('led')
-        counter +=  1
-        
-        client.check_msg()
-        
+
+
+        try:
+            client.check_msg()
+        except Exception as e:
+            print('chck_msg error: %r' % e)
+
         time.sleep(1)
-        printInfo()
+
+        if counter % KEEP_ALVIVE == 0:
+            client.ping()
+
+        counter += 1
+        print(counter)
+        #printInfo()
 
 if __name__ == '__main__':
     print('Running main.py')
-    
-    
+
+
     # set board time
     ntptime.settime()
 
     board = Board()
-    
-    client = MQTTClient('chickenMaster', config.broker)
+
+    client = MQTTClient('chickenMaster', config.broker,keepalive=KEEP_ALVIVE+5)
+    client.DEBUG = True
     client.set_callback(mqttCallback)
-    
+    client.set_last_will('tele/coop/LWT','Offline')
     client.connect()
-    
-    client.subscribe('coop/door/cmd')
+    client.publish('tele/coop/LWT', 'Online')
+    client.subscribe('cmnd/coop/DOOR')
 
     try:
         mainLoop(client,board)
     finally:
         client.disconnect()
-        
-    
